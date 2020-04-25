c3f3cb361d3c74a2766794d64d3692372e226dad
Fabilone Santos da Silva <fabilonesantos@hotmail.com>
2020-04-24 22:53:55 -0300

    Estruturação express

--- estudo-2/src/operation.js
+++ estudo-2/src/operation.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).send({
-        mensagem: 'OK, deu certo!'
-    });
-});
+const rotaProdutos = require('./routes/usuarios');
+
+app.use('/usuarios', rotaProdutos);
 
 module.exports = app;
---
+++ estudo-2/src/routes/usuarios.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) =>{
+    res.status(200).send({
+        mensagem: 'Usando o GET dentro da rota usuário'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o POST dentro da rota usuário'
+    });
+});
+
+module.exports = router;
