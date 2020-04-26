2038eab1cef3754c817ae768ba19679a4800aaf9
Fabilone Santos da Silva <fabilonesantos@hotmail.com>
2020-04-26 00:11:14 -0300

    Implementação de Delete e Patch

--- estudo-2/src/routes/usuarios.js
+++ estudo-2/src/routes/usuarios.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+
+//RETORNA TODOS OS USUÁRIOS
 router.get('/', (req, res, next) =>{
     res.status(200).send({
         mensagem: 'Usando o GET dentro da rota usuário'
     });
 });
 
+//INSERE UM USUÁRIO
 router.post('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Usando o POST dentro da rota usuário'
     });
 });
 
+//RETORNA OS DADOS DE UM USUÁRIO
 router.get('/:id_usuario', (req, res, next) => {
     const id = req.params.id_usuario;
 
@@ -27,6 +31,20 @@ router.get('/:id_usuario', (req, res, next) => {
             mensagem:  'você passou um Id'
         });
     }
+});
+
+//FAZENDO UM PATCH
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o PATCH dentro da rota de usuário'
+    })
+});
+
+//DELETANDO UM USUÁRIOS
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o DELETE dentro da rota usuário'
+    })
 });
 
 
