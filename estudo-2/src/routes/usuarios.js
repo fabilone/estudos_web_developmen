6981f94bf35a6791ce62e8acfdaa59b6fad037fa
Fabilone Santos da Silva <fabilonesantos@hotmail.com>
2020-04-25 23:55:22 -0300

    Passagem parametros usando Id

--- estudo-2/src/routes/usuarios.js
+++ estudo-2/src/routes/usuarios.js
@@ -14,9 +14,19 @@ router.post('/', (req, res, next) => {
 });
 
 router.get('/:id_usuario', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Usando o GET dentro da rota usuário. Buscando usuário especifico'
-    });
+    const id = req.params.id_usuario;
+
+    if(id === 'especial'){
+        res.status(201).send({
+            mensagem: 'Você encontrou o Id especial',
+            id: id
+        });
+
+    }else{
+        res.status(200).send({
+            mensagem:  'você passou um Id'
+        });
+    }
 });
 
 
